c3b350be5412cb09b3eeeb24158ee3eac07c4974
kprohaszka <cc.kprohaszka@gmail.com>
2021-02-23 07:42:54 -0800

    Create get_time function

--- util.py
+++ util.py
@@ -1,7 +1,11 @@
-from data_manager import get_saved_data
+import time
 
 
 def question_picker(question_id, questions):
     for question in questions:
         if question['id'] == question_id:
             return question
+
+
+def get_time():
+    return int(time.time())
