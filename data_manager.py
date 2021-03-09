0bd3451673522b1e4759bdebafdd4da4ba6d75b6
gabriellaszigeti <gbszgt@gmail.com>
2021-03-09 18:42:57 +0100

    recreated display a question

--- data_manager.py
+++ data_manager.py
@@ -2,12 +2,40 @@ from typing import List, Dict
 
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
+from datetime import datetime
 
+datedata = datetime.now().strftime("%Y-%m-%d %H:%M")
 import database_common
 
+
 @database_common.connection_handler
-def valami_funkció(cursor: RealDictCursor) -> list:
+def get_questions(cursor: RealDictCursor ) -> list:
     query = """
-    """
+        SELECT *
+        FROM question
+        ORDER BY id;"""
     cursor.execute(query)
     return cursor.fetchall()
+
+
+@database_common.connection_handler
+def get_question(cursor: RealDictCursor, question_id: int) -> list:
+    query = """
+        SELECT * 
+        FROM question
+        WHERE id = {};""".format(question_id)
+    cursor.execute(query)
+    return cursor.fetchone()
+
+
+@database_common.connection_handler
+def get_answer(cursor: RealDictCursor, question_id: int) -> list:
+    query = """
+        SELECT id, message, submission_time, vote_number
+        FROM answer
+        WHERE question_id = {}
+        ORDER BY id""".format(question_id)
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
